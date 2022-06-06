4b56e8d8b530c47b360ef11c66a6a932ead950b7
Stévillis Monteiro de Sousa <stevillis.sousa@neuralmind.ai>
2022-06-06 16:15:55 -0400

    settings for image upload

--- library/urls.py
+++ library/urls.py
@@ -13,12 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-
-from rest_framework import routers
-
 from books.api import viewsets
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+from rest_framework import routers
 
 route = routers.DefaultRouter()
 route.register('books', viewsets.BooksViewSet, basename='Books')
@@ -26,4 +26,4 @@ route.register('books', viewsets.BooksViewSet, basename='Books')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(route.urls)),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
