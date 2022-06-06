7686fc91eaeb0eda61dc059f84255d3f6fa8a89e
Stévillis Monteiro de Sousa <stevillis.sousa@neuralmind.ai>
2022-06-06 16:12:12 -0400

    add image field

--- books/models.py
+++ books/models.py
@@ -1,7 +1,15 @@
 from uuid import uuid4
+
 from django.db import models
 
+
+def upload_image_book(instance, filename):
+    """Define image file name"""
+    return f'{instance.id_book}-{filename}'
+
+
 class Book(models.Model):
+    """Book Model"""
     id_book = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
@@ -10,3 +18,5 @@ class Book(models.Model):
     pages = models.IntegerField()
     publishing_company = models.CharField(max_length=255)
     create_at = models.DateField(auto_now_add=True)
+    image = models.ImageField(
+        upload_to=upload_image_book, null=True, blank=True)
